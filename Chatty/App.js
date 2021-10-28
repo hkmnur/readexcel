3fecc5de9303917655e3ed6ef7cdce9189a66154
hkmnur <hnerkcan98@gmail.com>
2021-10-28 12:13:45 +0300

    ChatList

--- Chatty/App.js
+++ Chatty/App.js
@@ -8,26 +8,43 @@ import  Settings  from './screens/Settings';
 import SignUp from './screens/SignUp';
 import SignIn from './screens/SignIn';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {Ionicons} from '@expo/vector-icons'
+import {Provider} from 'react-native-paper'
 
 const Stack=createNativeStackNavigator();
 
 const Tabs=createBottomTabNavigator();
 
-const TabsNavigator = () =>{
-    <Tabs.Navigator>
+const TabsNavigator = () =>(
+    <Tabs.Navigator screenOptions={({route}) => ({
+      tabBarIcon:({ focused ,color,size}) => {
+          return <Ionicons name={route.name=== "ChatList" ?"chatbubbles" :"settings"}
+           color={color} 
+           size={size}/>;
+      }
+
+    })}>
         <Tabs.Screen name="ChatList" component={ChatList}/>
         <Tabs.Screen name="Settings" component={Settings}/>
     </Tabs.Navigator>
-}
+)
 
 const App = () => {
   return(
     <NavigationContainer>
-      <Stack.Navigator>
-          <Stack.Screen name="Main" component={TabsNavigator}/>
-      </Stack.Navigator>
+      <Provider>
+        <Stack.Navigator>
+            <Stack.Screen 
+              name="Main" 
+              component={TabsNavigator}
+              options={{headerShown:false}}
+            />
+            <Stack.Screen name="Chat" component={Chat}/>
+            <Stack.Screen name="SignUp" component={SignUp}/>
+            <Stack.Screen name="SignIn" component={SignIn}/>
+        </Stack.Navigator>
+      </Provider>
     </NavigationContainer>
   )
 };
-
 export default App;
--- Chatty/screens/ChatList.js
+++ Chatty/screens/ChatList.js
@@ -1,9 +1,30 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text ,View} from "react-native";
+import { List,Avatar,Divider,FAB,Portal,Dialog,Button } from "react-native-paper";
 
 const ChatList = () =>{
     return(
-        <Text>Hi,ChatList!</Text>
+        <View style={{flex:1}}>
+            <List.Item
+                title="User Name"
+                description="Hi, I will be waiting for you"
+                left={() => <Avatar.Text label="UN" size={56}/>}
+            />
+            <Divider inset/> 
+            <Portal>
+                <Dialog visible={true}>
+                    <Dialog.Title>New Chat</Dialog.Title>
+                    <Dialog.Content>
+                    
+                    </Dialog.Content>
+                    <Dialog.Actions>
+                        <Button>Done</Button>
+                    </Dialog.Actions>
+
+                </Dialog>
+            </Portal>
+            <FAB icon="plus" style={{position:'absolute',bottom:16,right:16}}/>
+        </View>
     )
 }
 
